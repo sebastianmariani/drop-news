84a193204a311174b15107ef9c9eda388d4ca7fc
User <sebastian.mariani.94@gmail.com>
2021-03-26 09:40:03 +0000

    adapt different screen size on newspage

--- src/Context/ContextData.js
+++ src/Context/ContextData.js
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 export const DataContext = createContext();
 
 export const DataProvider = props => {
-    const [searchCriteria, setSearchCriteria] = useState('');
+    const [searchCriteria, setSearchCriteria] = useState('general');
     const [country, setCountry] = useState('gb');
 
     return (
